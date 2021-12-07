cf6fefd70a355d1f16ff8b6c0317f01fc3e6d85e
maladjuka <69646840+maladjuka@users.noreply.github.com>
2021-12-07 13:04:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+/*let ham=document.getElementById('ham')
+ham.addEventListener('click',()=>{
+
+    let dropdown=document.getElementById('dropdown');
+    dropdown.classList.toggle('show')
+    
+                  })*/
+
+
+
+                
